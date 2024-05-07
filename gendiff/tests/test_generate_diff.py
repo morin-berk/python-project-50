6024a28682b952eb2de398424de7a7bab1b9182b
Kseniia Bekisheva <kseniia_bekisheva@epam.com>
2024-05-07 22:48:30 +0300

    Fix FileNotFound error in tests

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -5,40 +5,40 @@ import os
 
 @pytest.fixture
 def data_positive():
-    file1, file2 = "fixtures/file1.json", "fixtures/file2.json"
-    # file1_path, file2_path = os.path.abspath(file1), os.path.abspath(file2)
-    # return file1_path, file2_path
+    file1, file2 = "gendiff/tests/fixtures/file1.json", \
+                   "gendiff/tests/fixtures/file2.json"
     return file1, file2
-
 
 
 @pytest.fixture
 def data_equal_files():
-    file1 = "fixtures/file1.json"
+    file1 = "gendiff/tests/fixtures/file1.json"
     return file1, file1
 
 
 @pytest.fixture
 def data_different_files():
-    file1, file2 = "fixtures/file1.json", "fixtures/file3.json"
+    file1, file2 = "gendiff/tests/fixtures/file1.json", \
+                   "gendiff/tests/fixtures/file3.json"
     return file1, file2
 
 
 def test_positive(data_positive):
-    # result_path = os.path.abspath("tests/fixtures/result_positive.txt")
-    # result = open(result_path)
-    result = open("tests/fixtures/result_positive.txt")
+    result_path = os.path.abspath("gendiff/tests/fixtures/result_positive.txt")
+    result = open(result_path)
     file1, file2 = data_positive
     assert generate_diff(file1, file2) == result.read()
 
 
 def test_equal_files(data_equal_files):
-    result = open("fixtures/result_equal.txt")
+    result_path = os.path.abspath("gendiff/tests/fixtures/result_equal.txt")
+    result = open(result_path)
     file1, file2 = data_equal_files
     assert generate_diff(file1, file2) == result.read()
 
 
 def test_completely_different_files(data_different_files):
-    result = open("fixtures/result_different.txt")
+    result_path = os.path.abspath("gendiff/tests/fixtures/result_different.txt")
+    result = open(result_path)
     file1, file2 = data_different_files
     assert generate_diff(file1, file2) == result.read()
